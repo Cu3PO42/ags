68c72895d42f40a4750b3b1b35fbe6856e3a1684
Cu3PO42 <cu3po42@gmail.com>
2025-03-10 20:54:53 +0100

    chore: update nixpkgs

--- nix/default.nix
+++ nix/default.nix
@@ -13,7 +13,7 @@
 , gjs
 , wrapGAppsHook
 , upower
-, gnome
+, gnome-bluetooth
 , gtk-layer-shell
 , glib-networking
 , networkmanager
@@ -87,7 +87,7 @@ stdenv.mkDerivation rec {
     gtk3
     libpulseaudio
     upower
-    gnome.gnome-bluetooth
+    gnome-bluetooth
     gtk-layer-shell
     glib-networking
     networkmanager
